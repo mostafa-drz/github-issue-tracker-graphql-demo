a8a39d47fe8a7981a77676ed17d97fcc6680d4e9
mostafa.drz <mostafa69d@gmail.com>
2020-07-05 15:38:14 -0400

    init

---
+++ src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="main-container">
+      <h1>Github Issue Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
